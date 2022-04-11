ecabf76eec29fd9d89847962113cb7d83ed4a4b6
ChaseCone <chasecone@icloud.com>
2022-04-11 09:30:37 -0500

    Placing Towers

--- Assets/Scripts/PlacementManager.cs
+++ Assets/Scripts/PlacementManager.cs
@@ -5,9 +5,13 @@ using UnityEngine.UI;
 
 public class PlacementManager : MonoBehaviour
 {
-    public GameObject cube;
+    public GameObject cannon1;
+    public GameObject crossbow1;
+    public GameObject farm1;
+    public GameObject mage;
     public GameObject shopUI;
     public GameObject infoUI;
+    ClickDetector currScript;
 
     public Button buyFarm;
     public Button buyCrossbow;
@@ -22,7 +26,11 @@ public class PlacementManager : MonoBehaviour
     // Start is called before the first frame update
     void Start()
     {
-        
+        exitShop.onClick.AddListener(ExitShop);
+        buyFarm.onClick.AddListener(BuyFarm);
+        buyCannon.onClick.AddListener(BuyCannon);
+        buyCrossbow.onClick.AddListener(BuyCrossbow);
+        buyMage.onClick.AddListener(BuyMage);
     }
 
     // Update is called once per frame
@@ -33,8 +41,9 @@ public class PlacementManager : MonoBehaviour
 
     public void PromptShop(GameObject clicked)
     {
-        ClickDetector refScript = clicked.GetComponent<ClickDetector>();
+        currScript = clicked.GetComponent<ClickDetector>();
         shopUI.SetActive(true);
+
         //refScript.currTower = ClickDetector.TowerType.cannon;
         //PlaceTower(clicked);
     }
@@ -44,8 +53,57 @@ public class PlacementManager : MonoBehaviour
 
     }
 
-    public void PlaceTower(GameObject clicked)
+    public void PlaceTower(GameObject clicked, ClickDetector.TowerType towerType)
     {
-        GameObject tower = Instantiate(cube, clicked.transform.position, clicked.transform.rotation);
+        GameObject tower;
+        if (towerType == ClickDetector.TowerType.cannon)
+        {
+            tower = Instantiate(cannon1, clicked.transform.position, clicked.transform.rotation);
+            tower.transform.Rotate(23, 180, 0, Space.World);
+        }
+        else if (towerType == ClickDetector.TowerType.farm)
+        {
+            tower = Instantiate(farm1, clicked.transform.position, clicked.transform.rotation);
+            tower.transform.Rotate(23, 180, 0, Space.World);
+        }
+        else if (towerType == ClickDetector.TowerType.crossbow)
+        {
+            tower = Instantiate(crossbow1, clicked.transform.position, clicked.transform.rotation);
+            tower.transform.Rotate(23, 180, 0, Space.World);
+        }
+        else if (towerType == ClickDetector.TowerType.mage)
+        {
+            tower = Instantiate(mage, clicked.transform.position, clicked.transform.rotation);
+            tower.transform.Rotate(-67, 180, 0, Space.World);
+        }
+    }
+
+    private void ExitShop()
+    {
+        shopUI.SetActive(false);
+    }
+
+    private void BuyFarm()
+    {
+        PlaceTower(currScript.gameObject, ClickDetector.TowerType.farm);
+        shopUI.SetActive(false);
+    }
+
+    private void BuyCannon()
+    {
+        PlaceTower(currScript.gameObject, ClickDetector.TowerType.cannon);
+        shopUI.SetActive(false);
+    }
+
+    private void BuyCrossbow()
+    {
+        PlaceTower(currScript.gameObject, ClickDetector.TowerType.crossbow);
+        shopUI.SetActive(false);
+    }
+
+    private void BuyMage()
+    {
+        PlaceTower(currScript.gameObject, ClickDetector.TowerType.mage);
+        shopUI.SetActive(false);
     }
 }
