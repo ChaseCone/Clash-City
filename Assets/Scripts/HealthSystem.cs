0f51ee7749afe96b2cc0f1b8f06752075502319d
Tarun Joseph <101302761+Prune-Juice@users.noreply.github.com>
2022-04-19 10:45:32 -0500

    revamping health code

--- Assets/Scripts/HealthSystem.cs
+++ Assets/Scripts/HealthSystem.cs
@@ -1,5 +1,8 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
 
-public class HealthSystem
+public class HealthSystem : MonoBehaviour
 {
     private int health;
    public HealthSystem(int health)
@@ -15,10 +18,26 @@ public class HealthSystem
     public void Damage(int damageAmount)
     {
         health -= damageAmount;
+        if (health < 0)
+        {
+            health = 0;
+        }
     }
 
     /*public void Heal(int healAmount)
     {
         health += healAmount;
     }*/
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+
+    }
 }
