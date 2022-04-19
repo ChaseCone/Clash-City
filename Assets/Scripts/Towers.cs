95f32ce1983e63ee11f568169483d897440eecfc
JC2rj <Jackychen817@gmail.com>
2022-04-19 10:18:37 -0500

    Simple Tower Script
    Craetes Projectile rn

---
+++ Assets/Scripts/Towers.cs
@@ -0,0 +1,29 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Towers : MonoBehaviour
+{
+    public GameObject ProjectilePrefab;
+    public float fireRate =3f;
+    public float Cooldown =2f;
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+
+        if (Cooldown<=0f)
+        {
+            Instantiate(ProjectilePrefab, transform.position, ProjectilePrefab.transform.rotation);
+            Cooldown = 1f / fireRate;
+
+        }
+        Cooldown -= Time.deltaTime;
+    }
+
+}
