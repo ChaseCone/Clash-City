95555a14c6116ee1932b19e306e7723480d10a81
Tarun Joseph <101302761+Prune-Juice@users.noreply.github.com>
2022-05-17 10:51:32 -0500

    start of testing health

--- Assets/Scripts/TowerManager.cs
+++ Assets/Scripts/TowerManager.cs
@@ -9,6 +9,9 @@ public class TowerManager : MonoBehaviour
     // Start is called before the first frame update
     void Start()
     {
+        Transform bar = transform.Find("Healthbar");
+        HealthSystem hs = bar.GetComponent<HealthSystem>();
+        hs.Setup(this);
         health = maxHealth;
     }
 
